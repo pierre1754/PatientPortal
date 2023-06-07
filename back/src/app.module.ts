54d704ffe4e065df746f133a3613cf81df623b1c
pierre.hamel <pierre1754@outlook.com>
2023-06-08 00:43:45 +0200

    feat: added db docker compose and env file

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  // TODO: env var for mongo url
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest')],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
