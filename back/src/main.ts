562cccfe855a16b54a0751c4c6fc3d2c1c2e2b66
pierre.hamel <pierre1754@outlook.com>
2023-06-09 23:32:21 +0200

    feat: added and configure swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Patient Portal API')
+    .setDescription(
+      'Patient Portal is a web application that list patients and their treatments.',
+    )
+    .setVersion('1.0')
+    .addTag('Patient')
+    .addTag('Doctor')
+    .addTag('Treatment')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.BACKEND_PORT || 3000);
 }
+
 bootstrap();
